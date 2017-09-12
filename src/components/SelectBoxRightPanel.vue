84a0fd4eb7fa832ba6288abdb5b8b53780373d2a
sky-ADMIN <546485299@qq.com>
2017-09-12 22:47:12 +0800

    update

--- src/components/SelectBoxRightPanel.vue
+++ src/components/SelectBoxRightPanel.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="right-panel-container">
     <h1 class="list-title">已选择 <span>0</span>/<span>10</span>个标签</h1>
-    <div class="first-list">
-      <ul class="cas-panel">
-        <li class="cas-item" v-for="(item, index) in data.selectList" :class="{'tab-active': index == data.activeIndex}" v-on:click="handleCateClick(item, index)">
-          {{item.name}}
-        </li>
-      </ul>
+    <div v-if="data.level == 2">
+      <div class="first-list">
+        <ul class="cas-panel">
+          <li class="cas-item" v-for="(item, index) in data.selectList"
+              :class="{'tab-active': index == data.activeIndex}" v-on:click="handleCateClick(item, index)"
+          >
+            {{item.name}}
+          </li>
+        </ul>
+      </div>
+      <div class="second-list">
+        <ul class="cas-panel">
+          <li class="cas-item" v-for="(item, index) in data.selectList[data.activeIndex].list">
+            <input type="checkbox" :value="item.id" v-model="item.selected" :id="item.id"
+                   v-on:click="handleItemClick(item, index)"><label class="label" :for="item.id">{{item.name}}</label>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="second-list">
+    <div v-if="data.level == 1">
       <ul class="cas-panel">
-        <li class="cas-item" v-for="(item, index) in data.selectList[data.activeIndex].list">
-          <input type="checkbox" :value="item.id" v-model="item.selected" :id="item.id" v-on:click="handleItemClick(item, index)"><label class="label" :for="item.id">{{item.name}}</label>
+        <li class="cas-item" v-for="(item, index) in data.selectList">
+          <input type="checkbox" :value="item.id" v-model="item.selected" :id="item.id" v-on:click="handle1LevelItemClick(item, index)"><label class="label" :for="item.id">{{item.name}}</label>
         </li>
       </ul>
     </div>
@@ -33,10 +45,20 @@
       },
       handleItemClick(item, index) {
         let rs = {};
+        rs.level = 5;
         rs.name = item.name;
         rs.id = item.id;
         rs.itemIndex = index;
         rs.fourthCate = this.data.activeIndex;
+        console.log('change item: ' + JSON.stringify(rs));
+        this.$emit('select-change', rs);
+      },
+      handle1LevelItemClick(item, index) {
+        let rs = {};
+        rs.level = 4;
+        rs.name = item.name;
+        rs.id = item.id;
+        rs.itemIndex = index;
         console.log('change item: ' + JSON.stringify(rs));
         this.$emit('select-change', rs);
       }
@@ -61,7 +83,9 @@
     display: inline-block;
   }
 
-  .first-list::-webkit-scrollbar{width: 0}
+  .first-list::-webkit-scrollbar {
+    width: 0
+  }
 
   .second-list {
     float: right;
@@ -69,5 +93,7 @@
     display: inline-block;
   }
 
-  .second-list::-webkit-scrollbar{width: 0}
+  .second-list::-webkit-scrollbar {
+    width: 0
+  }
 </style>
